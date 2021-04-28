378d19b879105f4104a2ee8cfefe0fee65d6f8b4
Nikolay Ivanov <kolyan41k01@yandex.ru>
2021-04-28 22:33:53 +0300

    Настройка webpack

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+//Vendors
+import $ from 'jquery';
+window.$ = $;
+
+
+const app = {
+    ready() {
+        
+    },
+
+    load() {
+
+    },
+
+    resize() {
+
+    },
+
+    scroll() {
+
+    },
+};
+
+
+
+$(() => {
+    app.ready();
+
+    $(window)
+        .on('load', app.load)
+        .on('resize', app.resize)
+        .on('scroll', app.scroll);
+});
